4ffcf6bad76114399b6c4551b22a891af7753ff1
Madhuri-dev123 <gunnapananimadhuri@gmail.com>
2023-12-23 22:29:36 +0530

    m jn

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Counter from './Counter';
+import Todolist from './Todolist';
+function App() {
+  return (
+    <div className="mybox">
+      <h1>Welcom to ReactJs</h1>
+      <Counter></Counter>
+      <Todolist></Todolist>
+    </div>
+  );
+}
+
+export default App;
