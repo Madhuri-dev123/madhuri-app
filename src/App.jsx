22370ea4f8744cc75d0c18f172fdd59ed186fc5f
Madhuri-dev123 <gunnapananimadhuri@gmail.com>
2023-12-25 15:21:45 +0530

    n mfm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import React from 'react';
 import './App.css';
-import Counter from './Counter';
 import Todolist from './Todolist';
 function App() {
+  console.log("first line")
+  function abc(){
+    console.log("abc")
+    setC(234);
+  }
+  var [c,setC]=React.useState(1)
   return (
     <div className="mybox">
       <h1>Welcom to ReactJs</h1>
-      <Counter></Counter>
+      <h1>{c}</h1>
+      <button onClick={()=>{abc()}} >increment</button>
       <Todolist></Todolist>
     </div>
   );
--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -3,21 +3,29 @@ function Todolist(){
     //state
     var [todos,setTodos]=React.useState(["get car","buy new house"])
     //logic
+    function addTodo(){
+        var nt = document.getElementById("d1").value;
+        setTodos([todos,nt])
+    }
     //templete
     return(
-        <div > 
-            <h1 className="mybox">Todolist</h1>
+        <>
+        <div className="mybox"> 
+            <h1>Todolist</h1>
+            <input type="text" id="d1"></input>
+            <button onClick={()=>{addTodo()}}>Add Todo</button>
             <ul>
                 
                     {
 
-                        todos.map((todo)=>{
-                          return<li>{todo}</li>
+                        todos.map((todo,i)=>{
+                          return<li key={i}>{todo}</li>
                         })
                     }
                 
             </ul>
         </div>
+        </>
     )
 }
-export default Todolist;
+export default React.memo(Todolist);
